ac0ba677cd18e36571d4970e9d1acf0296cae8c6
Cedric DJIVO <cedric_dji@yahoo.fr>
2024-05-28 00:33:32 +0200

    Fix main tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ provider "aws" {
   region = var.region
 }
 resource "aws_instance" "server" {
-  ami = "ami-0306ddf5a2c963a25"
+  ami = "ami-00ac45f3035ff009e"
   instance_type = "t2.micro"
   key_name = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
