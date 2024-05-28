94fbbe658967d30f25d9ca4c7426174012ffd36a
Cedric DJIVO <cedric_dji@yahoo.fr>
2024-05-28 17:36:05 +0200

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,6 +82,6 @@ resource "aws_key_pair" "deployer" {
 }
 
 output "instance_public_ip" {
-  value = aws_instance.servernode.public_ip
+  value = aws_instance.server.public_ip
   sensitive = true
 }
